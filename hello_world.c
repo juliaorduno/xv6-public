e5480303a4becab3308f3a1eb22955aaf7c4a842
julia paola <juliapaola97@gmail.com>
2017-10-04 19:17:56 -0500

    Implemented semaphore for mutex

--- hello_world.c
+++ hello_world.c
@@ -1,12 +1,24 @@
 #include <pthread.h>
 #include <stdio.h>
-#define NUM_THREADS 5
+#include <unistd.h>
+#include <semaphore.h>
+#define NUM_THREADS 10
+
+long saldo;
+sem_t mutex;
 
 void *PrintHello(void *threadid)
 {
     long tid;
+    long lsaldo;
     tid = (long)threadid;
     printf("Hello World! It's me, thread #%ld!\n", tid);
+    sem_wait(&mutex);
+    lsaldo = saldo;
+    sleep(1); //Forzando a perder el cpu
+    lsaldo += 1;
+    saldo = lsaldo;
+    sem_post(&mutex);
     pthread_exit(NULL);
 }
 
@@ -15,6 +27,10 @@ int main(int argc, char *argv[])
     pthread_t threads[NUM_THREADS];
     int rc;
     long t;
+
+    saldo = 0;
+    sem_init(&mutex,0,1);
+
     for (t = 0; t < NUM_THREADS; t++)
     {
         printf("In main: creating thread %ld\n", t);
@@ -26,6 +42,11 @@ int main(int argc, char *argv[])
         }
     }
 
+    for (t = 0; t < NUM_THREADS; t++)
+    {
+        pthread_join(threads[t], NULL);
+    }
+    printf("El valor del saldo es %ld\n", saldo);
     /* Last thing that main() should do */
     pthread_exit(NULL);
 }
